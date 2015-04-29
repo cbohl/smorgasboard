e906cd455629bd7132e1ce4fa52dc63fe9e195b4
Alex Knoll <alexandergknoll@gmail.com>
2015-04-29 15:26:36 -0500

    comment tests to test travis

--- spec/features/games_spec.rb
+++ spec/features/games_spec.rb
@@ -1,46 +1,46 @@
 require 'rails_helper'
 
 RSpec.feature "Games", type: :feature do
-  context "when a user is not signed in" do
-    scenario "when a user visits the log in page" do
-      visit '/login'
-      expect(page).to have_content("Log In")
-    end
-    scenario "when a user visits the log in page" do
-      visit '/login'
-      expect(page).to have_content("Sign Up")
-    end
-    scenario "when a user visits the home page" do
-      visit '/'
-      expect(current_path).to eq '/login'
-    end
-    scenario "when a user visits the games index" do
-      visit '/games'
-      expect(current_path).to eq '/login'
-    end
-    scenario "when a user visits a game show page" do
-      visit '/games/1'
-      expect(current_path).to eq '/login'
-    end
-    scenario "when a user visits the users index" do
-      visit '/users'
-      expect(current_path).to eq '/login'
-    end
-    scenario "when a user visits a user show page" do
-      visit '/users/1'
-      expect(current_path).to eq '/login'
-    end
-    scenario "when a user visits the tags index" do
-      visit '/tags'
-      expect(current_path).to eq '/login'
-    end
-    scenario "when a user visits a tags show page" do
-      visit '/tags/1'
-      expect(current_path).to eq '/login'
-    end
-    scenario "when a user visits a new comment page" do
-      visit '/games/1/comments/new'
-      expect(current_path).to eq '/login'
-    end
-  end
+  # context "when a user is not signed in" do
+  #   scenario "when a user visits the log in page" do
+  #     visit '/login'
+  #     expect(page).to have_content("Log In")
+  #   end
+  #   scenario "when a user visits the log in page" do
+  #     visit '/login'
+  #     expect(page).to have_content("Sign Up")
+  #   end
+  #   scenario "when a user visits the home page" do
+  #     visit '/'
+  #     expect(current_path).to eq '/login'
+  #   end
+  #   scenario "when a user visits the games index" do
+  #     visit '/games'
+  #     expect(current_path).to eq '/login'
+  #   end
+  #   scenario "when a user visits a game show page" do
+  #     visit '/games/1'
+  #     expect(current_path).to eq '/login'
+  #   end
+  #   scenario "when a user visits the users index" do
+  #     visit '/users'
+  #     expect(current_path).to eq '/login'
+  #   end
+  #   scenario "when a user visits a user show page" do
+  #     visit '/users/1'
+  #     expect(current_path).to eq '/login'
+  #   end
+  #   scenario "when a user visits the tags index" do
+  #     visit '/tags'
+  #     expect(current_path).to eq '/login'
+  #   end
+  #   scenario "when a user visits a tags show page" do
+  #     visit '/tags/1'
+  #     expect(current_path).to eq '/login'
+  #   end
+  #   scenario "when a user visits a new comment page" do
+  #     visit '/games/1/comments/new'
+  #     expect(current_path).to eq '/login'
+  #   end
+  # end
 end
