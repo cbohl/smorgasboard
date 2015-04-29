72d25e772e4425e74bec2b38c9b4c93d88085f1c
John Dees <jdees08@gmail.com>
2015-04-29 17:56:25 -0500

    updated the model relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :ratings
 	has_many :comments
-	has_many :users_games
+	has_many :user_games, class_name: "UserGame", foreign_key: :user_id
+  has_many :owned_games, through: :user_games, source: :game
 	has_many :rated_games, through: :ratings, source: :game
 	has_many :commented_on_games, through: :comments, source: :game
 	has_many :games, through: :users_games
