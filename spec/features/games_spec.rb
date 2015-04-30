7b08c4a305443161e69c68b73411fcbdee2aaba3
joshzich <zichjd@gmail.com>
2015-04-30 14:21:38 -0500

    got rid of extra tests/ passing existing tests

--- spec/features/games_spec.rb
+++ spec/features/games_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "User redirected to home page", type: :feature do
+  let!(:user) { User.create!(name: "John", email: "jdees@gmail.com", username: "jdees", password: "password") }
   scenario "when user clicks link with text SmorgasBoard" do
     visit '/games'
     click_link('SmorgasBoard')
@@ -8,6 +9,10 @@ RSpec.feature "User redirected to home page", type: :feature do
   end
 
   scenario "when user clicks link with text home" do
+    visit '/login'
+    fill_in('Username', :with => user.username)
+    fill_in('Password', :with => user.password)
+    click_button('login')
     visit '/games'
     click_link('Home')
     expect(current_path).to eq '/'
@@ -15,10 +20,15 @@ RSpec.feature "User redirected to home page", type: :feature do
 end
 
 RSpec.feature "User directed to appropriate game", type: :feature do
+  let!(:user) { User.create!(name: "John", email: "jdees@gmail.com", username: "jdees", password: "password") }
   let! (:game) { Game.create(name: "Monopoly", description: "Really fun", min_number_of_players: 2, max_number_of_players: 4, min_duration_of_game: 90, max_duration_of_game: 180) }
   scenario "when user clicks link for the first game on the list" do
+    visit '/login'
+    fill_in('Username', :with => user.username)
+    fill_in('Password', :with => user.password)
+    click_button('login')
     visit '/games'
     click_link('Monopoly')
-    expect(current_path).to eq '/games/1'
+    expect(current_path).to eq '/games/8'
   end
 end
