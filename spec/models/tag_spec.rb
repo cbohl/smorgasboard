e2e0de6ea575848cf666090724c1320c59528af0
Christopher Bohl <chrisrbohl@gmail.com>
2015-04-30 13:33:09 -0500

    games_spec tests in progress

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -5,7 +5,7 @@ describe Tag do
   let! (:game_2) { Game.create(name: "Risk", description: "Really fun", min_number_of_players: 2, max_number_of_players: 4, min_duration_of_game: 90, max_duration_of_game: 180) }
   let! (:tagging_1) { Tagging.create!(game: game_1, tag: tag) }
   let! (:tagging_2) { Tagging.create!(game: game_2, tag: tag) }
-  let! (:tag) { Tag.create!(name: "Board Games", description: "Type") }
+  let! (:tag) {Tag.create!(name: "Board Games", description: "Type")}
 
   it "should have many Taggings" do
     expect(tag.taggings).to eq([tagging_1, tagging_2])
