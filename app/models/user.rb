11183f4064bdce93463fbb19ebf93ea979895bd7
John Dees <jdees08@gmail.com>
2015-04-29 20:24:29 -0500

    changed order and naming of some of the user relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ActiveRecord::Base
-	has_many :ratings
-	has_many :comments
 	has_many :user_games, class_name: "UserGame", foreign_key: :user_id
   has_many :owned_games, through: :user_games, source: :game
-	has_many :rated_games, through: :ratings, source: :game
-	has_many :commented_on_games, through: :comments, source: :game
-	has_many :games, through: :users_games
+  has_many :ratings
+  has_many :rated_games, through: :ratings, source: :game
+	has_many :comments
+  has_many :commented_games, through: :comments, source: :game
 
 	validates :email, presence: true
 	validates :username, presence: true
