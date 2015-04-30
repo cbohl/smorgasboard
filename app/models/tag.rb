35ff54fc21b38fd73b268afcd7b2c67873688e21
Alex Knoll <alexandergknoll@gmail.com>
2015-04-29 20:57:09 -0500

    tag association tests complete

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ActiveRecord::Base
 	has_many :taggings
-	has_many :tagged_games, through: :taggings, source: :games
+	has_many :tagged_games, through: :taggings, source: :game
 
 	validates :name, presence: true
 end
