17c055cbc3a4ccd4025df2d730f17c893258a411
Christopher Bohl <chrisrbohl@gmail.com>
2015-04-30 11:42:07 -0500

    Feature testing changes, database_cleaner gem added to gemfile and used in rails_helper

--- spec/features/games_spec.rb
+++ spec/features/games_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-<<<<<<< HEAD
+
 RSpec.feature "User redirected to home page", type: :feature do
   scenario "when user clicks link with text SmorgasBoard" do
     visit '/games'
@@ -12,12 +12,13 @@ RSpec.feature "User redirected to home page", type: :feature do
     click_link('Home')
     expect(current_path).to eq '/'
   end
+end
 
-
-
-=======
-require 'spec_helper'
->>>>>>> 3ec96a993380c61a696963a054bba2f46cadc227
-
-
+RSpec.feature "User directed to appropriate game", type: :feature do
+  let! (:game) { Game.create(name: "Monopoly", description: "Really fun", min_number_of_players: 2, max_number_of_players: 4, min_duration_of_game: 90, max_duration_of_game: 180) }
+  scenario "when user clicks link for the first game on the list" do
+    visit '/games'
+    click_link('Monopoly')
+    expect(current_path).to eq '/games/1'
+  end
 end
