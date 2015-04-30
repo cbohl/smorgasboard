f068dda76ff805572ffd856a9a601d4bbdafe789
John Dees <jdees08@gmail.com>
2015-04-30 14:56:27 -0500

    added the tags stuff

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,12 @@
 class TagsController < ApplicationController
 	def index
+    @mechanics = Tag.where(category: "mechanic")
+    @genres = Tag.where(category: "genre")
 	end
 
 	def show
+    puts params
+    @tag = Tag.find_by(id: params[:id])
+    @games = @tag.tagged_games
 	end
 end
