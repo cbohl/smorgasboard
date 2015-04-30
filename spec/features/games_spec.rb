11317546184b7fb8d9ae04b374a6880d4458167e
Christopher Bohl <chrisrbohl@gmail.com>
2015-04-30 10:48:17 -0500

    2 tests done for features games_spec

--- spec/features/games_spec.rb
+++ spec/features/games_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
+RSpec.feature "User redirected to home page", type: :feature do
+  scenario "when user clicks link with text SmorgasBoard" do
+    visit '/games'
+    click_link('SmorgasBoard')
+    expect(current_path).to eq '/'
+  end
 
-RSpec.feature "Games", type: :feature do
+  scenario "when user clicks link with text home" do
+    visit '/games'
+    click_link('Home')
+    expect(current_path).to eq '/'
+  end
+
+
+
+
 
 end
