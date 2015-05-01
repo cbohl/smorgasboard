91e495751d42337d13402ca9a3b4ce2dd2230511
Alex Knoll <alexandergknoll@gmail.com>
2015-05-01 16:45:24 -0500

    purge commented controller code

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,23 +1,12 @@
 class GamesController < ApplicationController
 
   def index
-
-    # @games Game.all.select{|game| game unless game.average_rating.nan?}
     @games = Game.all.select{|game| game unless game.average_rating.nan?}.sort_by(&:average_rating).reverse[0..9]
-
     @prefixes = ('A'..'Z').to_a
     if params[:letter]
       @games = Game.where("games.name LIKE :letter", { letter: params[:letter]}).page(params[:page]).per(5)
     end
-    # if request.xhr?
-    #   render '_alphabetized_games', layout: false
-    # end
   end
-
-  # def alphabetized_games
-  #   @prefixes = ('A'..'Z').to_a
-  #   @games = Game.where("games.name LIKE :letter", { letter: params[:letter]}).page(params[:page]).per(5)
-  # end
 
   def show
     @user = current_user
