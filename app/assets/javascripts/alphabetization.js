678208d2cc36e4458ed6556f792e046b6ae799ae
joshzich <zichjd@gmail.com>
2015-05-01 15:47:22 -0500

    yaaay done

---
+++ app/assets/javascripts/alphabetization.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+// 	$('.letter-link').on('click', function(event){
+// 		event.preventDefault();
+// 		$('#top-ten-games').remove();
+// 		alphaBoot = $.ajax({
+// 			url: '/games',
+// 			type: 'get' 
+// 		})
+// 		alphaBoot.done(function(response){
+// 			console.log(response);
+// 			$('body').append(response);
+// 		})
+// 	})
+// });
